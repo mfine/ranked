63332ea3736d6c001acacee1636fd9aecedd5e7a
Blake Gentry <blakesgentry@gmail.com>
2012-05-01 10:08:17 -0700

    proper display names

--- lib/ranked/result.rb
+++ lib/ranked/result.rb
@@ -1,16 +1,24 @@
 module Ranked
   class Result < Sequel::Model
 
+    def loser
+      @loser ||= Player[loser_id]
+    end
+
+    def winner
+      @winner ||= Player[winner_id]
+    end
+
     def winner_name
-      Player[winner_id].name
+      winner.name
     end
 
     def loser_name
-      Player[loser_id].name
+      loser.name
     end
 
     def opponent(player)
-      [winner_name, loser_name].reject { |p| p == player.name }.first
+      [winner, loser].find { |p| p != player }.display_name
     end
 
     def result(player)
