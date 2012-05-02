34319b4b7e3fe6052a971166702a20e12cda42de
Blake Gentry <blakesgentry@gmail.com>
2012-05-02 12:00:31 -0700

    Just log instead of failing when CAMPFIRE_URL isn't set

--- lib/ranked/campfire.rb
+++ lib/ranked/campfire.rb
@@ -3,6 +3,8 @@ module Ranked
 
     def self.say(msg)
       `curl --silent -H 'Content-Type: application/json' -d '{\"message\": {\"body\": \"#{msg}\"}}' #{Conf.campfire_url} > /dev/null` if Conf.campfire_url
+    rescue MissingConf
+      Log.notice("not logging to campfire because CAMPFIRE_URL is not set")
     end
 
     def self.say_result(result)
