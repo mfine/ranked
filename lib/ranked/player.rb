9c0d0a85f722e3183012c25d0b8aa3fc0caf2d9d
Mark Fine <mark.fine@gmail.com>
2012-05-04 13:00:09 -0700

    Add gravatar.

--- lib/ranked/player.rb
+++ lib/ranked/player.rb
@@ -1,9 +1,15 @@
+require "digest/md5"
+
 module Ranked
   class Player < Sequel::Model
     attr_accessor :rating
 
     def display_name
       name || user
+    end
+
+    def gravatar
+      "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(name)}.png"
     end
   end
 end
