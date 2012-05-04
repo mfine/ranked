33f22e9b56fdb3a9bfaaa03a19c769f2f0ed446d
Mark Fine <mark.fine@gmail.com>
2012-05-04 13:12:32 -0700

    Add gravatar.

--- lib/ranked/player.rb
+++ lib/ranked/player.rb
@@ -9,7 +9,7 @@ module Ranked
     end
 
     def gravatar
-      "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user)}.png?s=64"
+      "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user)}.png?s=32"
     end
   end
 end
