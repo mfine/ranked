4daf7760e5a923b512c01148d7ee641a6eabadba
Blake Gentry <blakesgentry@gmail.com>
2012-05-08 17:35:17 -0700

    Put active players first in dropdown

--- lib/ranked/player.rb
+++ lib/ranked/player.rb
@@ -11,5 +11,20 @@ module Ranked
     def gravatar
       "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user)}.png?s=20&d=mm"
     end
+
+    def self.active_ids
+      recent = Ranked::Result.filter{at > Time.at(Time.now.to_i - 2*7*24*60*60)}.
+        select_map([:winner_id, :loser_id]).
+        flatten.
+        uniq
+    end
+
+    def self.active
+      filter(id: active_ids)
+    end
+
+    def self.inactive
+      exclude(id: active_ids)
+    end
   end
 end
